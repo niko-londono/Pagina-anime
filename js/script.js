03fa87f17132f88068ba0bc3b4fb41048505599a
Nicolas Londoño Hernandez <92286646+niko-londono@users.noreply.github.com>
2023-02-28 23:00:21 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,15 +3,29 @@ $(document).ready(function(){
         var value = $(this).val().toLowerCase();
         $('#search-list a').filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-
-        // if ($("#search-input").val() == "")
-        // {
-        //     $(".search-container").fadeOut(300);
-        // }
-        // else
-        // {
-        //     $(".search-container").fadIn(300);
-        // }
+        });        
     });
 });
+
+//CALCULADORA
+function multiplicar() {
+    var duracion = 23; //duracion de anime
+    var cantidadEP = document.getElementById("cantidadEP").value;
+    var resultado = duracion *  cantidadEP;
+    document.getElementById("resultado").value = "Minutos: " + resultado;
+    document.getElementById('minAnime').value = "Duracion: " +duracion;
+}
+  
+function resetear()
+{
+    var cantidadEP = document.getElementById('cantidadEP').value;
+    var resultado = document.getElementById('resultado').value;
+
+    if (resultado != "")
+    {
+        document.getElementById('nombre').value = "";
+        document.getElementById('cantidadEP').value = "";
+        document.getElementById('resultado').value = "";
+        document.getElementById('minAnime').value = "";
+    }
+}
